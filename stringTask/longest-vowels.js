233357df2f494a2496421fd8c24e0cb1bb3cf247
Paula Lopes <pauladev21@gmail.com>
2022-03-17 20:32:27 -0300

    Refactored js code and included testing pages design

--- stringTask/longest-vowels.js
+++ stringTask/longest-vowels.js
@@ -1,53 +1,50 @@
- const text = "Given a sentence with multiple lowercase words separated by spaces, write a Javascript code that finds the longest word in the sentence. If there are multiple words of the same length, choose one that has the highest number of vowels. Ignore any character in the sentence that is not a letter or a space. Find the most efficient way to achieve this."
+ /*const text = "Given:
+ - a sentence with multiple lowercase words separated by spaces, 
+ - write a Javascript code that finds the longest word in the sentence. 
+ - If there are multiple words of the same length, 
+ -- choose one that has the highest number of vowels. 
+ - Ignore any character in the sentence that is not a letter or a space. 
+ - Find the most efficient way to achieve this."
+ */
 
-/*const text = "Smart people learn from everything and everyone, average people from their experience, everything again, stupidity, stupid people already, have all the answers\" (Socrates 15:25)"*/
+const text = "Smart people learn from everything and everyone, average people from their experience, everything again, stupidity, stupid people already, have all the answers\" (Socrates)";
 
 console.log(findLongestVowelsWord(text));
 
 function findLongestVowelsWord(str){   
-    let words = findLongests(str);
+    let words = findLongests(str.toLowerCase());
+    if (words.length == 1){
+        return words[0];
+    }
     let vowelsCounts = words.map(countVowels);
-    maxVowelsIndex = vowelsCounts.reduce(findMaxVowelsIndex,-1);
+    //console.log(vowelsCounts)
+    let maxVowelsIndex = vowelsCounts.reduce(findMaxIndex,0);
     return words[maxVowelsIndex];
 }
 
-function listWords(str){
-        str = str.replace(/\d+/g,' ');
-        let words = str.match(/\w+\s+|\w+/g);
-        words = words.map(trimWord);
-        return words;
-}
-
-function trimWord(w){
-    return w.trim();
-}
-
 function findLongests(str){
-    let words = listWords(str);
-    let largestSize=0;
-    largestSize = words.reduce(findLongestSize, 0);
-    return words.filter(getLongestOnly, largestSize);
+    let words = findWords(str);   
+    let longestSize = words.reduce(findLongestSize,0);
+    words = words.filter(removeOldLongests(longestSize));
+    return removeRepeated(words);
 }
 
-function countVowels(arr){
-    let vowels = arr.match(/[aeiou]/g);
-    return vowels.length;
-}
-
-function getLongestOnly(word){
-    return word.length === this.valueOf();
-}
-
-function findMaxVowelsIndex(previousValue, currentValue, index){ 
-    if (currentValue > previousValue){
-        return index;
+function removeRepeated(words){
+    let index=0, wordsTail=[];
+    while (index < words.length-1){
+        wordsTail=words.slice(index+1,);
+        if (wordsTail.indexOf(words[index]) >=0 ){
+            words.shift();
+        }
+        else{
+            index++;
+        }        
     }
-    else{
-        return previousValue;
-    }
+    return words;
 }
+   
 
-function findLongestSize(previousValue, currentValue){  
+function findLongestSize(previousValue, currentValue){     
     if (currentValue.length >= previousValue){
         return currentValue.length;
     }
@@ -55,6 +52,42 @@ function findLongestSize(previousValue, currentValue){
         return previousValue;
     }  
 }
+
+function findWords(str){
+    str = str.replace(/\d+/g,' ');
+    let words = str.match(/\w+\s+|\w+/g);
+    words = words.map(trimWord);    
+    return words;
+}
+
+function trimWord(word){
+    return word.trim();
+}
+
+function removeOldLongests(maxSize){
+    return function(word){
+        return (word.length === maxSize);
+    };
+}
+
+function countVowels(word){
+    let vowels = word.match(/[aeiou]/g);
+    return vowels.length;
+}
+
+
+function findMaxIndex(previousMaxIndex, currentElem, arrIndex, array){ 
+    console.log(previousMaxIndex, currentElem, arrIndex, array)
+
+    if (currentElem > array[previousMaxIndex]){
+        return arrIndex;
+    }
+    else{
+        return previousMaxIndex;
+    }
+}
+
+
 
 
 
